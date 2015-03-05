3f78457872bed890722c205c504bf6d80f6bef6a
Marshall Pierce <marshall@mpierce.org>
2015-03-04 17:59:58 -0800

    quick take on a reservoir that resets on every snapshot

---
+++ src/main/java/org/mpierce/metrics/reservoir/hdrhistogram/HdrHistogramResetOnSnapshotReservoir.java
@@ -0,0 +1,74 @@
+package org.mpierce.metrics.reservoir.hdrhistogram;
+
+import com.codahale.metrics.Reservoir;
+import com.codahale.metrics.Snapshot;
+import org.HdrHistogram.Histogram;
+import org.HdrHistogram.Recorder;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * A Reservoir that resets its internal state every time a snapshot is taken. This is useful if you're using snapshots
+ * as a means of defining the window in which you want to calculate, say, the 99.9th percentage.
+ */
+@ThreadSafe
+public final class HdrHistogramResetOnSnapshotReservoir implements Reservoir {
+
+    private final Recorder recorder;
+
+    @GuardedBy("this")
+    @Nonnull
+    private Histogram intervalHistogram;
+
+    /**
+     * Create a reservoir with a default recorder. This recorder should be suitable for most usage.
+     */
+    public HdrHistogramResetOnSnapshotReservoir() {
+        this(new Recorder(2));
+    }
+
+    /**
+     * Create a reservoir with a user-specified recorder.
+     *
+     * @param recorder Recorder to use
+     */
+    public HdrHistogramResetOnSnapshotReservoir(Recorder recorder) {
+        this.recorder = recorder;
+
+        /*
+         * Start by flipping the recorder's interval histogram.
+         * - it starts our counting at zero. Arguably this might be a bad thing if you wanted to feed in
+         *   a recorder that already had some measurements? But that seems crazy.
+         * - intervalHistogram can be nonnull.
+         * - it lets us figure out the number of significant digits to use in runningTotals.
+         */
+        intervalHistogram = recorder.getIntervalHistogram();
+    }
+
+    @Override
+    public int size() {
+        // This appears to be infrequently called, so not keeping a separate counter just for this.
+        return getSnapshot().size();
+    }
+
+    @Override
+    public void update(long value) {
+        recorder.recordValue(value);
+    }
+
+    @Override
+    public Snapshot getSnapshot() {
+        return new HistogramSnapshot(updateRunningTotals());
+    }
+
+    /**
+     * @return a copy of the accumulated state
+     */
+    @Nonnull
+    private synchronized Histogram updateRunningTotals() {
+        intervalHistogram = recorder.getIntervalHistogram(intervalHistogram);
+        return intervalHistogram.copy();
+    }
+}
